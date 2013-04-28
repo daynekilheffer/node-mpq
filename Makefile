b1b621b1edd6bcad4070aa670712f8c4478f9056
daynekilheffer <dayne.kilheffer@gmail.com>
2013-04-28 14:43:59 -0400

    added mocha watch and factory tests

--- Makefile
+++ Makefile
@@ -3,4 +3,7 @@ TESTS = test/*.js
 test:
 	@NODE_ENV=test ./node_modules/.bin/mocha --reporter spec $(TESTS)
 
+test-watch:
+	@NODE_ENV=test ./node_modules/.bin/mocha --reporter spec -w $(TESTS)
+
 .PHONY: test
--- test/factory.js
+++ test/factory.js
@@ -1,31 +1,19 @@
 var should = require('should');
-var root = require('../lib');
+var factory = require('../lib').FileParserFactory;
 var EventEmitter = require('events').EventEmitter;
 
-var factory = root.FileParserFactory;
 describe('factory', function() {
-    it('should have a factory method', function() {
-        should.exist(factory.createParser);
-        factory.createParser.should.be.a('function');
-    })
-    it('should create a parser', function() {
-        var parser = factory.createParser('filename');
-        should.exist(parser); 
-    })
-    it('should create a parser which is a pseudo EventEmitter', function() {
-        var parser = factory.createParser();
-        should.exist(parser.on);
-    })
+	describe('api', function() {
+		it('should have a factory method', function() {
+			should.exist(factory.createParser);
+			factory.createParser.should.be.a('function');
+		})
+	})
+	describe('#createParser', function() {
+		it('should create a parser', function() {
+			var parser = factory.createParser('filename');
+			should.exist(parser);
+		})
+	})
 })
 
-var parser = factory.createParser();
-describe('parser', function() {
-    it('should have a parse method', function() {
-        should.exist(parser.parse);
-        parser.parse.should.be.a('function');
-    })
-    it('should have an extract method', function() {
-        should.exist(parser.extract);
-        parser.extract.should.be.a('function');
-    })
-})
